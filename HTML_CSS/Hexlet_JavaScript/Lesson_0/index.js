87a2169dfbc3c98f79bf74ecf0defb98a2c800fa
Ilnur Sirazhev <code_gex@mail.ru>
2024-02-20 16:48:07 +0700

    Выполение заданий

--- HTML_CSS/Hexlet_JavaScript/Lesson_0/index.js
+++ HTML_CSS/Hexlet_JavaScript/Lesson_0/index.js
@@ -1,2 +1,6 @@
-console.log("Hello World");
-console.log(8/2+5 - -3 / 2);
+// console.log("Hello World");
+// console.log(8/2+5 - -3 / 2);
+// let who = "dragon's" + 'mother';
+// console.log(who);
+const name1 = 'Robb';
+console.log(name1[name1.length]);

0e79534575d4da594f7d23d050f5a3c62318899f
Ilnur Sirazhev <code_gex@mail.ru>
2024-03-15 17:03:44 +0700

    Classes & Objects

--- PHP/programwithgio/src/public/index.php
+++ PHP/programwithgio/src/public/index.php
@@ -1,7 +1,41 @@
 <?php
 
-//phpinfo();
+// Classes & Objects
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
+//declare(strict_types=1);
+//
+//require_once '../Transaction.php';
+//
+//$transaction = (new Transaction(100, 'Transaction 1'))
+//    ->addTax(8)
+//    ->applyDiscount(10);
+//
+//$amount = $transaction->getAmount();
+//
+//$transaction = null;
+//
+//exit;
+//
+//var_dump($amount);
+
+//$str = '{"a":1,"b":2,"c":3}';
+//
+//$arr = json_decode($str);
+//
+//var_dump($arr->a);
+
+//$obj = new \stdClass();
+//
+//$obj->a = 1;
+//$obj->b = 2;
+//
+//var_dump($obj);
+
+//$arr = [1,2,3];
+//$obj = (object)$arr;
+//
+//var_dump($obj->{0});
+
+$obj = (object) null;
+
+var_dump($obj);

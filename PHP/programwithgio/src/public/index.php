aa79886a3a30a1298cd0e6a5e73eedffc182d424
Ilnur Sirazhev <code_gex@mail.ru>
2024-03-16 14:34:27 +0700

    Constructor Property Promotion

--- PHP/programwithgio/src/public/index.php
+++ PHP/programwithgio/src/public/index.php
@@ -1,41 +1,11 @@
 <?php
 
-// Classes & Objects
+// Constructor Property Promotion
 
-//declare(strict_types=1);
-//
-//require_once '../Transaction.php';
-//
-//$transaction = (new Transaction(100, 'Transaction 1'))
-//    ->addTax(8)
-//    ->applyDiscount(10);
-//
-//$amount = $transaction->getAmount();
-//
-//$transaction = null;
-//
-//exit;
-//
-//var_dump($amount);
+require_once '../PaymentProfile.php';
+require_once '../Customer.php';
+require_once '../Transaction.php';
 
-//$str = '{"a":1,"b":2,"c":3}';
-//
-//$arr = json_decode($str);
-//
-//var_dump($arr->a);
+$transaction = new Transaction(5,'Test');
 
-//$obj = new \stdClass();
-//
-//$obj->a = 1;
-//$obj->b = 2;
-//
-//var_dump($obj);
-
-//$arr = [1,2,3];
-//$obj = (object)$arr;
-//
-//var_dump($obj->{0});
-
-$obj = (object) null;
-
-var_dump($obj);
+echo $transaction->getCustomer()?->getPaymentProfile()?->id;

4c9dafc9dfaf1d7e3bceed4383d1e863bdd4681e
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-04 17:01:36 +0700

    WC3school/PHP Start

---
+++ PHP/wc3schools/websites/Demo/index.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+//echo 'Hello Brothers!!!';
+//echo strrev("Hello World!");
+//$oldtxt = "Hello World!";
+//$newtxt = str_replace('World','Dolly',$oldtxt);
+//echo $newtxt;
+//$i = 1;
+//
+//do {
+//    echo $i;
+//    $i++;
+//} while ($i < 6);
+
+//$colors = array("red", "green", "blue", "yellow");
+//
+//foreach ($colors as $x) {
+//    echo $x;
+//}
+
+//function myFunction()
+//{
+//    echo "Hello World!";
+//}
+//$fruits = array("Apple", "Banana", "Orange");
+//var_dump($fruits);
+
+//$x = 5;
+//echo '<pre>';
+//print_r($GLOBALS);
+//echo '<pre>';
+//echo $GLOBALS['x'];
+
+//echo '<h2>PHP is Fun!</h2>';
+//echo "Hello world!<br>";
+//echo "I'm about to learn PHP!<br>";
+//echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+//$cars = array('VAZ', 'TAZ', 'GAZ', 'BELAZ');
+//var_dump($cars);
+
+//class Car
+//{
+//    public string $color;
+//    public string $model;
+//
+//    public function __construct(string $color, string $model)
+//    {
+//        $this->color = $color;
+//        $this->model = $model;
+//    }
+//
+//    public function message(): string
+//    {
+//        return 'My car is a ' . $this->color . ' ' . $this->model . '!';
+//    }
+//}
+//
+//$myCar = new Car('Brown', 'Camry');
+//echo $myCar->message();
+
+//$x = 'Hello Honey!';
+//$x = null;
+//var_dump($x);
+
+//echo strlen('Hello') . '<pre>';
+//echo strlen('Привет') . '<pre>';
+//echo mb_strlen('Привет') . '<pre>';
+//echo str_word_count('How are you doing ?');
+//echo strpos('How are you doing ?', '?');
+//$x = 'Hi Bro!';
+//echo strtoupper($x);
+//echo strtolower($x);
+//echo str_replace('Bro', 'Sister', $x);
+//echo strrev($x);
+//echo trim($x);
+//$y = explode(' ', $x);
+//print_r($y);
+
+//$x = PHP_INT_MAX+1;
+//echo substr($x, -3);
+//echo substr($x, -5, 5);
+//$x = 1.9e411;
+//var_dump($x);
+
+//$x = acos(8);
+//var_dump($x);
+
+//$a = 5;       // Integer
+//$b = 5.34;    // Float
+//$c = "hello"; // String
+//$d = true;    // Boolean
+//$e = NULL;    // NULL
+//
+//$a = (string) $a;
+//$b = (string) $b;
+//$c = (string) $c;
+//$d = (string) $d;
+//$e = (string) $e;
+//
+////To verify the type of any object in PHP, use the var_dump() function:
+//var_dump($a);
+//echo '<pre>';
+//var_dump($b);
+//echo '<pre>';
+//var_dump($c);
+//echo '<pre>';
+//var_dump($d);
+//echo '<pre>';
+//var_dump($e);
+
+//$a = 5;       // Integer
+//$b = 5.34;    // Float
+//$c = "25 kilometers"; // String
+//$d = "kilometers 25"; // String
+//$e = "hello"; // String
+//$f = true;    // Boolean
+//$g = NULL;    // NULL
+//
+//$a = (int) $a;
+//$b = (int) $b;
+//$c = (int) $c;
+//$d = (int) $d;
+//$e = (int) $e;
+//$f = (int) $f;
+//$g = (int) $g;
+//
+//var_dump($a);
+//var_dump($b);
+//var_dump($c);
+//var_dump($d);
+//var_dump($e);
+//var_dump($f);
+//var_dump($g);
+
+echo 'Hello';

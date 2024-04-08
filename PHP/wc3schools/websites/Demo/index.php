d1087fdcc1f680afa94a9e11bb79e7d43a2906f4
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-08 17:11:05 +0700

    WC3schools/PHP DAY 3

--- PHP/wc3schools/websites/Demo/index.php
+++ PHP/wc3schools/websites/Demo/index.php
@@ -2,57 +2,40 @@
 
 declare(strict_types=1);
 
+// WC3schools/PHP DAY 3
 
-//$colors = ['red', 'white', 'blue', 'yellow'];
-//
-//foreach ($colors as $color) {
-//    echo $color . '<br>';
-//}
 
-//$members = ['Joe' => '22', 'Maria' => 23, 'Ilya' => 37, 'Ainur' => 32];
+//$cars = array('BMW' => 'Bucket', 'Mercedes' => 'Bath', 'Audi' => 'Pool');
 //
-//foreach ($members as $name => $age) {
-//    echo $name . ' is ' . $age . ' years old<br>';
-//}
-
-//class Cars
-//{
-//    public string $model;
-//    public string $color;
-//
-//    public function __construct($color, $model)
-//    {
-//        $this->color = $color;
-//        $this->model = $model;
-//    }
-//}
-//
-//$myCar = new Cars('green', 'priora');
-//
-//foreach ($myCar as $item => $value) {
-//    echo $item . ' ' . $value . '<br>';
-//}
-
-//function printMessage(string $name): void
-//{
-//    echo 'Yoo ' . $name . '! How are you?';
-//}
-//
-//printMessage('George');
-//printMessage('Vasiliy');
-//printMessage('Nikolay');
-
-//$cars = array('BMW', 'Mercedes', 'Audi');
-$cars = array('BMW' => 'Bucket', 'Mercedes' => 'Bath', 'Audi' => 'Pool');
-//echo end($cars);
-//array_push($cars, 1, 2, 3, 4, 'a', '2', ['b'=>'WWW']);
-//$cars[] = [1, 2, 3, 4, 'a', '2', ['b'=>'WWW']];
+//$carsToCase = array_change_key_case($cars,CASE_UPPER);
 //
 //echo '<pre>';
-//var_dump($cars);
+//var_dump($GLOBALS['cars']);
 //echo '<pre>';
 
-$carsToCase = array_change_key_case($cars,CASE_UPPER);
-echo '<pre>';
-var_dump($carsToCase);
-echo '<pre>';
+//$x = 75;
+//
+//function myfunction() {
+//    echo $x;
+//}
+//
+//myfunction();
+
+//echo $_SERVER['PHP_SELF'] . '<br>';
+//echo $_SERVER['SERVER_NAME'] . '<br>';
+//echo $_SERVER['HTTP_HOST'] . '<br>';
+////echo $_SERVER['HTTP_REFERER'] . '<br>';
+//echo $_SERVER['HTTP_USER_AGENT'] . '<br>';
+//echo $_SERVER['SCRIPT_NAME'];
+
+//echo '<pre>';
+//print_r($_SERVER);
+//echo '<pre>';
+
+//echo '<pre>';
+//print_r($_REQUEST['firstname']);
+//echo '<pre>';
+
+$str = 'Welcome to the CLUB buddy!!';
+$pattern = '/club/i';
+echo preg_match($pattern,$str);

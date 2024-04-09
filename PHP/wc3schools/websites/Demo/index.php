67bbf8dca1894ece34651e780f4d0b15cb59793a
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-09 17:24:07 +0700

    WC3schools/PHP DAY 4

--- PHP/wc3schools/websites/Demo/index.php
+++ PHP/wc3schools/websites/Demo/index.php
@@ -2,40 +2,29 @@
 
 declare(strict_types=1);
 
-// WC3schools/PHP DAY 3
+// WC3schools/PHP DAY 4
 
+//echo readfile('testfile.txt');
+//$myFile = fopen('testfile.txt', "r") or die('Unable to open file!');
+//echo fread($myFile, filesize('testfile.txt'));
+//echo fgets($myFile);
+//echo fgets($myFile);
 
-//$cars = array('BMW' => 'Bucket', 'Mercedes' => 'Bath', 'Audi' => 'Pool');
-//
-//$carsToCase = array_change_key_case($cars,CASE_UPPER);
-//
-//echo '<pre>';
-//var_dump($GLOBALS['cars']);
-//echo '<pre>';
-
-//$x = 75;
-//
-//function myfunction() {
-//    echo $x;
+//while (!feof($myFile)){
+//    echo fgets($myFile) . '<br>';
 //}
-//
-//myfunction();
 
-//echo $_SERVER['PHP_SELF'] . '<br>';
-//echo $_SERVER['SERVER_NAME'] . '<br>';
-//echo $_SERVER['HTTP_HOST'] . '<br>';
-////echo $_SERVER['HTTP_REFERER'] . '<br>';
-//echo $_SERVER['HTTP_USER_AGENT'] . '<br>';
-//echo $_SERVER['SCRIPT_NAME'];
+//while (!feof($myFile)) {
+//    echo fgetc($myFile);
+//}
+//echo fgetc($myFile);
+//echo fgetc($myFile);
+//fclose($myFile);
 
-//echo '<pre>';
-//print_r($_SERVER);
-//echo '<pre>';
-
-//echo '<pre>';
-//print_r($_REQUEST['firstname']);
-//echo '<pre>';
-
-$str = 'Welcome to the CLUB buddy!!';
-$pattern = '/club/i';
-echo preg_match($pattern,$str);
+$newFile = fopen('newfile.txt','a') or die('Unable to open file!');
+$txt = 'Donald Duck' . '<br>';
+fwrite($newFile, $txt);
+$txt = 'Goofy Goof' . '<br>';
+fwrite($newFile, $txt);
+fclose($newFile);
+echo readfile('newfile.txt');

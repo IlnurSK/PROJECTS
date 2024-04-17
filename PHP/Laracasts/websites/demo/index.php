709f138ede0d52dc2d8b94a0080371e55a861a26
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-17 16:46:59 +0700

    Refactoring/Partials

--- PHP/Laracasts/websites/demo/index.php
+++ PHP/Laracasts/websites/demo/index.php
@@ -1,32 +1,5 @@
 <?php
 
-declare(strict_types=1);
+$heading = "Home";
 
-// Separate PHP Logic From the Template
-
-$books = [
-    [
-        "name" => "Do Androids Dream of Electric Sheep",
-        'author' => 'Philip K. Dick',
-        'releaseYear' => 1968,
-        'purchaseUrl' => 'http://example.com'
-    ],
-    [
-        "name" => "Project Hail mary",
-        'author' => 'Andy Weir',
-        'releaseYear' => 2021,
-        'purchaseUrl' => 'http://example.com'
-    ],
-    [
-        "name" => "The Martian",
-        'author' => 'Andy Weir',
-        'releaseYear' => 2011,
-        'purchaseUrl' => 'http://example.com'
-    ],
-];
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['releaseYear'] >= 1950 && $book['releaseYear'] <= 2020;
-});
-
-require "index.view.php";
+require "views/index.view.php";

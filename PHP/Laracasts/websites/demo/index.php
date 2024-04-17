f31ce283a4f86961e98eae2d808d003ffe51f32d
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-17 14:23:37 +0700

    Laracasts - Conditionals and Booleans

--- PHP/Laracasts/websites/demo/index.php
+++ PHP/Laracasts/websites/demo/index.php
@@ -1,17 +1,36 @@
-<!--Laracast - VARIABLES-->
+<!--Laracasts - Conditionals and Booleans-->
 
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+
+<?php
+$name = "Dark Matter";
+$read = false;
+
+if ($read) {
+    $message = "You have read $name";
+} else {
+    $message = "You have not read $name";
+}
+?>
+
 <h1>
-    <?php
-    $greeting = 'Hello, ';
-    echo $greeting . 'World';
-    ?>
+    <?= $message ?>
 </h1>
+
 </body>
 </html>

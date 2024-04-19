d0ebe7271e5297c60acaff0c87db8558e74cf251
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-19 17:00:02 +0700

    PDO connection to MySQL DB

--- PHP/Laracasts/websites/demo/index.php
+++ PHP/Laracasts/websites/demo/index.php
@@ -1,4 +1,35 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+//class Person {
+//    public $name;
+//    public $age;
+//
+//    public function breathe()
+//    {
+//        echo $this->name . " is breathing!";
+//    }
+//}
+//
+//$person = new Person();
+//
+//$person->name = 'John Doe';
+//$person->age = 25;
+//
+//$person->breathe();
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root', 'root');
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
+

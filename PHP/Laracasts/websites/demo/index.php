54f216999990c3cc7c5c0b237ea8981fc925b100
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-20 15:23:23 +0700

    SQL Injections

--- PHP/Laracasts/websites/demo/index.php
+++ PHP/Laracasts/websites/demo/index.php
@@ -5,10 +5,12 @@ require 'Database.php';
 //require 'router.php';
 
 $config = require 'config.php';
-
 $db = new Database($config['database']);
 
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
 

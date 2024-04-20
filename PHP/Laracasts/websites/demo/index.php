26b853f976a389f077fb25c383ba24c7dac77f59
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-20 14:41:07 +0700

    Extract a PHP Database Class

--- PHP/Laracasts/websites/demo/index.php
+++ PHP/Laracasts/websites/demo/index.php
@@ -1,35 +1,14 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 //require 'router.php';
 
-//class Person {
-//    public $name;
-//    public $age;
-//
-//    public function breathe()
-//    {
-//        echo $this->name . " is breathing!";
-//    }
-//}
-//
-//$person = new Person();
-//
-//$person->name = 'John Doe';
-//$person->age = 25;
-//
-//$person->breathe();
 
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
 
-$pdo = new PDO($dsn, 'root', 'root');
+$db = new Database();
 
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
 

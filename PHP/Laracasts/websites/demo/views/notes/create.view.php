9f573e8b91ef719fd4c0c276b197bddf3b3ae896
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-21 16:44:27 +0700

    Resourceful Naming Conventions

--- PHP/Laracasts/websites/demo/views/notes/create.view.php
+++ PHP/Laracasts/websites/demo/views/notes/create.view.php
@@ -1,10 +1,9 @@
 <?php
-
-require('partials/head.php') ?>
+require('views/partials/head.php') ?>
 <?php
-require('partials/nav.php') ?>
+require('views/partials/nav.php') ?>
 <?php
-require('partials/banner.php') ?>
+require('views/partials/banner.php') ?>
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
@@ -45,4 +44,4 @@ require('partials/banner.php') ?>
 </main>
 
 <?php
-require('partials/footer.php') ?>
+require('views/partials/footer.php') ?>

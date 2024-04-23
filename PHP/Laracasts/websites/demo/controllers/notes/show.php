743e39fd9cad6fa55e6c3f9016bbf7d2918f53e2
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-23 16:02:52 +0700

    Delete Form

--- PHP/Laracasts/websites/demo/controllers/notes/show.php
+++ PHP/Laracasts/websites/demo/controllers/notes/show.php
@@ -5,17 +5,31 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$currentUserId = 1;
+$currentUserId = 22;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('SELECT * FROM notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('DELETE FROM notes WHERE id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
+    $note = $db->query('SELECT * FROM notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
 
 
-$note = $db->query('SELECT * FROM notes where id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
-
-authorize($note['user_id'] === $currentUserId);
-
-
-view("notes/show.view.php", [
-    'heading' => 'Notes',
-    'note' => $note
-]);
+    view("notes/show.view.php", [
+        'heading' => 'Notes',
+        'note' => $note
+    ]);
+}

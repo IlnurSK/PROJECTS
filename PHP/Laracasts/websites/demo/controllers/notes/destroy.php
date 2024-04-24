bf21063c2e814590a4d064e18771fb1c255ca34b
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-24 16:07:02 +0700

    One Request One Controller

--- PHP/Laracasts/websites/demo/controllers/notes/destroy.php
+++ PHP/Laracasts/websites/demo/controllers/notes/destroy.php
@@ -7,14 +7,16 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
+
 $note = $db->query('SELECT * FROM notes where id = :id', [
-    'id' => $_GET['id']
+    'id' => $_POST['id']
 ])->findOrFail();
 
 authorize($note['user_id'] === $currentUserId);
 
-
-view("notes/show.view.php", [
-    'heading' => 'Notes',
-    'note' => $note
+$db->query('DELETE FROM notes WHERE id = :id', [
+    'id' => $_POST['id']
 ]);
+
+header('location: /notes');
+exit();

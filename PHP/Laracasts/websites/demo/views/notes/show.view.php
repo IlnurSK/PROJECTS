a0e8104ff084d895e36af10a7061770a88e99a22
Ilnur Sirazhev <code_gex@mail.ru>
2024-04-25 15:27:31 +0700

    Updating with PATCH method

--- PHP/Laracasts/websites/demo/views/notes/show.view.php
+++ PHP/Laracasts/websites/demo/views/notes/show.view.php
@@ -1,4 +1,5 @@
 <?php
+
 require base_path('views/partials/head.php') ?>
 <?php
 require base_path('views/partials/nav.php') ?>
@@ -10,12 +11,17 @@ require base_path('views/partials/banner.php') ?>
         <p class="mb-6">
             <a href="/notes" class="text-blue-500 underline">Go back...</a>
         </p>
+
         <p><?= htmlspecialchars($note['body']) ?></p>
-        <form class="mt-6" method="post">
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="hidden" name="id" value="<?= $note['id'] ?>">
-            <button class="text-sm text-red-500">Delete</button>
-        </form>
+        <footer class="mt-6 gap-x-4">
+            <a href="/note/edit?id=<?= $note['id'] ?>" class="rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Edit</a>
+            <form class="mt-6" method="post">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                <button class="rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Delete</button>
+            </form>
+        </footer>
+
     </div>
 </main>
 

c8533e6e015c82930c7ff17e2de83bb8c2af0e48
Ilnur Sirazhev <code_gex@mail.ru>
2024-05-02 14:33:41 +0700

    Extract an Authenticator Class

--- PHP/Laracasts/websites/demo/Http/controllers/session/store.php
+++ PHP/Laracasts/websites/demo/Http/controllers/session/store.php
@@ -2,47 +2,33 @@
 
 // логиним пользователя если учетные данные совпадают
 
-use Core\App;
-use Core\Database;
-use Core\Validator;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
 
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
 
-if (!$form->validate($email, $password)) {
-    return view('session/create.view.php', [
-        'errors' => $form->errors()
-    ]);
+if ($form->validate($email, $password)) {
+    if ((new Authenticator)->attempt($email, $password)) {
+        redirect('/');
+    }
+
+    $form->error('email', 'No matching account found for that email and password.');
 };
 
 // проверяем соответствие учетных данных
 
-$user = $db->query('select * from users where email = :email', [
-    'email' => $email
-])->find();
-
-// у нас есть пользователь, но мы не знаем соответствует ли введенный пароль - паролю с нашей базы данных.
-
-if ($user) {
-    if (password_verify($password, $user['password'])) {
-        login([
-            'email' => $email
-        ]);
-
-        header('location: /');
-        exit();
-    }
-}
 
 return view('session/create.view.php', [
-    'errors' => [
-        'email' => 'No matching account found for that email and password.'
-    ]
+    'errors' => $form->errors()
 ]);
+
+
+
+
+
 
 

a09073125c5be56789fa5e576d8200e903b2f3e1
Ilnur Sirazhev <code_gex@mail.ru>
2024-05-02 15:40:56 +0700

    Flash Old Form Data to the Session

--- PHP/Laracasts/websites/demo/Core/functions.php
+++ PHP/Laracasts/websites/demo/Core/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Response;
+use Core\Session;
 
 function dd($value)
 {
@@ -47,6 +48,11 @@ function redirect($path)
 {
     header("location: {$path}");
     exit();
+}
+
+function old($key, $default = '')
+{
+        return Session::get('old')[$key] ?? $default;
 }
 
 

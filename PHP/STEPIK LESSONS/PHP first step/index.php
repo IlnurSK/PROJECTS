27dd872dbc5ba048acd9b8c89045105eb17a59f2
Ilnur Sirazhev <code_gex@mail.ru>
2024-10-07 15:13:16 +0500

    PHP BASE операции с массивами

--- PHP/STEPIK LESSONS/PHP first step/index.php
+++ PHP/STEPIK LESSONS/PHP first step/index.php
@@ -1,7 +1,4 @@
 <?php
 
-$firstStep = readline() + readline() + readline();
-$secondStep = $firstStep * readline();
-$thirdStep = $secondStep - ($secondStep % 10);
-$fourthStep = $thirdStep / 5;
-echo $fourthStep;
+$myarr = array(readline(),readline(),readline(),readline());
+echo $myarr[readline()];

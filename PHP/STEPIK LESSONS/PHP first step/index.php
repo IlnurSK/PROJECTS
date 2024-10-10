021d90c0ec1068534561f5fcceebc128e5d44156
Ilnur Sirazhev <code_gex@mail.ru>
2024-10-10 16:31:22 +0500

    PHP BASE задачи/функция/some_times

--- PHP/STEPIK LESSONS/PHP first step/index.php
+++ PHP/STEPIK LESSONS/PHP first step/index.php
@@ -1,21 +1,11 @@
 <?php
 
-function win_or_lose($num)
+function some_times(int $N, string $S)
 {
-    switch ($num) {
-        case 0:
-            return "Поражение";
-        case 3:
-            return "Победа";
-        case 1:
-            return "Ничья";
-        default:
-            return "Странное число";
+    for ($i = 0; $i < $N; $i++) {
+        echo $S . PHP_EOL;
     }
 }
 
-echo win_or_lose(3);
-echo win_or_lose(0);
-echo win_or_lose(1);
-echo win_or_lose(2);
+some_times(3,"Весна");
 

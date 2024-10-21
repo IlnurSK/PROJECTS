c236dd834fd4e12d17f78ce276e2181bc6485749
Ilnur Sirazhev <code_gex@mail.ru>
2024-10-21 15:14:32 +0500

    PHP BASE CHEATSHEET/Задача на конвертирование

--- PHP/STEPIK LESSONS/PHP first step/index.php
+++ PHP/STEPIK LESSONS/PHP first step/index.php
@@ -1,14 +1,8 @@
 <?php
 
-class Tree
-{
-    public $kind = "Дуб";
+$inputMeter = readline();
+$foot = $inputMeter / 0.3;
+$yard = $inputMeter / 0.9;
 
-    public function viewKind()
-    {
-        echo $this->kind;
-    }
-}
-
-$elk = new Tree();
-$elk->viewKind();
+echo "$inputMeter метра(ов) это $foot футов." . PHP_EOL;
+echo "$inputMeter метра(ов) это $yard ярдов.";

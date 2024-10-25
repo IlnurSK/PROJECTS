ee33bd9116f5c957d1f858ab961147a54e654fbe
Ilnur Sirazhev <code_gex@mail.ru>
2024-10-25 14:24:59 +0500

    PHP BASE CHEATSHEET/Задача на исправление кода

--- PHP/STEPIK LESSONS/PHP first step/index.php
+++ PHP/STEPIK LESSONS/PHP first step/index.php
@@ -1,16 +1,12 @@
 <?php
 
-$inputNum = readline();
-$countLastNums = readline();
-
-$calcNum = $inputNum % (10**$countLastNums);
-
-$resMul = 1;
-$resPlus = 0;
-$pointer = 0;
-for ($i = 1; $i <= $countLastNums; $i++) {
-    $pointer = (($calcNum % (10**$i)) - ($calcNum % (10**($i - 1))))/10**($i - 1);
-    $resMul *= $pointer;
-    $resPlus += $pointer;
+$x = 1;
+while ($x < 11)
+{
+    if ($x % 2 == 0) {
+        $x += 1;
+        continue;
+    }
+    echo $x . "\n";
+  $x += 1;
 }
-echo "$resPlus $resMul";

5e6e6caf356cdb265d36de1e1884dbfa11e47b64
Ilnur Sirazhev <code_gex@mail.ru>
2024-10-25 14:56:21 +0500

    PHP BASE CHEATSHEET/Задача на преобразование строки в массив

--- PHP/STEPIK LESSONS/PHP first step/index.php
+++ PHP/STEPIK LESSONS/PHP first step/index.php
@@ -1,19 +1,5 @@
 <?php
 
-$food = array(
-    "молоко" => 15,
-    "сыр" => 100,
-    "мясо" => 200,
-    "помидоры" => 30,
-    "картофель" => 20
-);
-
-echo '<pre>';
-print_r($food);
-echo '<pre>';
-
-$some_numbers = ["1", 2, 3.0];
-array_push($some_numbers, "four", 2);
-echo '<pre>';
-print_r($some_numbers);
-echo '<pre>';
+$numbers = "5 10 15 20 25";
+$array = explode(" ",$numbers);
+print_r($array);

8d56c6dd2c3f5e0487b78bc0900951796fd4c0d1
Ilnur Sirazhev <code_gex@mail.ru>
2024-11-11 15:43:19 +0500

    PHP BASE CHEATSHEET/Задача на отображение дней в месяце

--- PHP/STEPIK LESSONS/PHP first step/index.php
+++ PHP/STEPIK LESSONS/PHP first step/index.php
@@ -1,8 +1,26 @@
 <?php
 
-function triangle_square(int $length, int $height)
+function number_of_days(int $month)
 {
-    return 1/2 * $length * $height;
+    switch ($month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+        case 2:
+            return 28;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+        default:
+            return "Такого месяца нет";
+    }
 }
 
-echo triangle_square(11,4);
+echo number_of_days(0);

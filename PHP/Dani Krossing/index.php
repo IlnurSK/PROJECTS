e59a9ca750a770a0548b3b2cc53e142d35f706d9
Ilnur Sirazhev <code_gex@mail.ru>
2024-11-14 17:03:18 +0500

    Подготовка к тесту день 3 + регулярки

--- PHP/Dani Krossing/index.php
+++ PHP/Dani Krossing/index.php
@@ -1,6 +1,30 @@
 <?php
+
+//$array = array(0,1,2);
+//foreach ($array as $value) {
+//    $value += 2;
+//}
+//var_dump($array);
+
+//function Hello($a,$b)
+//{
+//    echo "Hello" . $a;
+//}
+//
+//Hello("Vasya");
+
+//$time = "5";
+//echo "PHP {$time}.6 conference at ${time} ";
+//выведет "PHP 5.6 conference at 5"
+//$time = "5 a.m. "
+//echo "PHP 5.6" . " conference at $time ";
+//пропущена ;
+//$time = "5 a.m.";
+//echo "PHP 5.6 " . " conference at " . "${time}" ;
+
 $string = "PHP56_testing";
 
 echo preg_match("/^[A-Za-z0-9]*$/", $string);
 echo preg_match("/56_test/", $string);
 echo preg_match("/^PHP(\w)*/", $string);
+

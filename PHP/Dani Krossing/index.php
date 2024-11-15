b7854ba6b80106f5abdaaf2c39ce169a0a07d44f
Ilnur Sirazhev <code_gex@mail.ru>
2024-11-15 17:46:06 +0500

    Подготовка к тесту GeekBrains на продвинутый уровень PHP/День 1

--- PHP/Dani Krossing/index.php
+++ PHP/Dani Krossing/index.php
@@ -27,4 +27,11 @@ $string = "PHP56_testing";
 echo preg_match("/^[A-Za-z0-9]*$/", $string);
 echo preg_match("/56_test/", $string);
 echo preg_match("/^PHP(\w)*/", $string);
+echo preg_match("/^(\w)$/", $string);
+echo preg_match("/[0-9]+$/", $string);
+//
+//echo array("Hello", "World");
+
+
+
 

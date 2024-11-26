f794e0e3373f7be88bf0009836503e141a1d8227
Ilnur Sirazhev <code_gex@mail.ru>
2024-11-26 17:19:36 +0500

    Практические задачи на наследование и интерфейсы/Refactoring

--- PHP/Dani Krossing/index.php
+++ PHP/Dani Krossing/index.php
@@ -156,111 +156,154 @@
 //echo "Собака по имени {$dog->getName()}: {$dog->speak()}" . "<br>";
 //echo "Кошка по имени {$cat->getName()}: {$cat->speak()}";
 
-//declare(strict_types=1);
-//abstract class Shape
-//{
-//    protected $color;
-//
-//    public function __construct(string $color)
-//    {
-//        $this->color = $color;
-//    }
-//
-//    // Метод для вычисления площади фигуры
-//    abstract function area(): float;
-//
-//    // Метод для форматирования площади с 2 знаками после запятой
-//    public function formatArea(): string
-//    {
-//        return number_format($this->area(), 2);
-//    }
-//}
-//interface Drawable
-//{
-//    // Метод для рисования фигуры
-//    public function draw();
-//
-//}
-//
-//class Rectangle extends Shape implements Drawable
-//{
-//    protected $width;
-//    protected $height;
-//
-//    public function __construct(string $color, float $width, float $height)
-//    {
-//        parent::__construct($color);
-//        $this->setWidth($width);
-//        $this->setHeight($height);
-//    }
-//
-//    public function area(): float
-//    {
-//        // Рассчитываем площадь прямоугольника
-//        return $this->width * $this->height;
-//    }
-//
-//    public function draw()
-//    {
-//        // Отображаем информацию о прямоугольнике и его площади
-//        echo "Прямоугольник, цвет: " . $this->color . ", площадь: " . $this->formatArea() . "<br>";
-//    }
-//
-//    public function setWidth(float $width)
-//    {
-//        if ($width < 0) {
-//            throw new InvalidArgumentException("Ширина должна быть неотрицательной.");
-//        }
-//        $this->width = $width;
-//        return $this;
-//    }
-//
-//    public function setHeight(float $height)
-//    {
-//        if ($height < 0) {
-//            throw new InvalidArgumentException("Высота должна быть неотрицательной.");
-//        }
-//        $this->height = $height;
-//        return $this;
-//    }
-//}
-//
-//class Circle extends Shape implements Drawable
-//{
-//    protected $radius;
-//    public function __construct(string $color, float $radius)
-//    {
-//        parent::__construct($color);
-//        $this->setRadius($radius);
-//    }
-//
-//    public function area(): float
-//    {
-//        // Рассчитываем площадь круга
-//        return pi() * pow($this->radius,2);
-//    }
-//
-//    public function draw()
-//    {
-//        // Отображаем информацию о круге и его площади
-//        echo "Круг, цвет: " . $this->color . ", площадь: " . $this->formatArea() . "<br>";
-//    }
-//
-//    public function setRadius(float $radius)
-//    {
-//        if ($radius < 0) {
-//            throw new InvalidArgumentException("Радиус должен быть неотрицательным.");
-//        }
-//        $this->radius = $radius;
-//        return $this;
-//    }
-//}
-//
-//// Создаем объекты и отображаем их
-//$rectangle = new Rectangle("красный", 10, 2);
-//$circle = new Circle("синий",3);
-//
-//$rectangle->draw();
-//$circle->draw();
+declare(strict_types=1);
 
+// Абстрактный класс Shape представляет собой общую основу для всех геометрических фигур
+abstract class Shape
+{
+    protected string $color; // Цвет фигуры
+
+    // Конструктор для установки цвета фигуры
+    public function __construct(string $color)
+    {
+        $this->color = $color; // Инициализация свойства $color
+    }
+
+    // Абстрактный метод для вычисления площади фигуры, который должен быть реализован в дочерних классах
+    abstract public function calculateArea(): float;
+
+    // Метод для форматирования площади с 2 знаками после запятой
+    public function formatArea(): string
+    {
+        // Возвращаем строковое представление площади, отформатированное с двумя знаками после запятой
+        return number_format($this->calculateArea(), 2);
+    }
+
+    // Метод для получения строкового представления объекта
+    public function __toString(): string
+    {
+        return json_encode([
+            'type' => get_class($this),
+            'color' => $this->color,
+            'area' => $this->formatArea(),
+        ]);
+    }
+
+    // Метод для получения цвета фигуры
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+}
+
+// Интерфейс для всех объектов, которые можно "нарисовать"
+interface Drawable
+{
+    // Метод для рисования фигуры (отображения)
+    public function draw();
+
+}
+
+// Класс Rectangle представляет прямоугольник и реализует интерфейс Drawable
+class Rectangle extends Shape implements Drawable
+{
+    protected float $width; // Ширина прямоугольника
+    protected float $height; // Высота прямоугольника
+
+    // Конструктор для создания прямоугольника с заданным цветом, шириной и высотой
+    public function __construct(string $color, float $width, float $height)
+    {
+        if ($width < 0 || $height < 0) {
+            throw new InvalidArgumentException("Ширина и высота должны быть неотрицательными.");
+        }
+        // Инициализация родительского класса для установки цвета фигуры
+        parent::__construct($color);
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    // Реализация абстрактного метода для вычисления площади прямоугольника
+    public function calculateArea(): float
+    {
+        return $this->width * $this->height;
+    }
+
+    // Реализация метода рисования прямоугольника
+    public function draw(): void
+    {
+        // Выводим строковое представление объекта (информацию о фигуре)
+        echo "Рисуем: " . get_class($this) . ", цвет: " . $this->color . ", площадь: " . $this->formatArea() . "<br>";
+    }
+
+    // Метод для установки ширины с проверкой на отрицательное значение
+    public function setWidth(float $width): void
+    {
+        if ($width < 0) {
+            // Выбрасываем исключение, если ширина отрицательная, так как это физически невозможно
+            throw new InvalidArgumentException("Ширина должна быть неотрицательной.");
+        }
+        $this->width = $width;
+    }
+
+    // Метод для установки высоты с проверкой на отрицательное значение
+    public function setHeight(float $height): void
+    {
+        if ($height < 0) {
+            // Выбрасываем исключение, если высота отрицательная, так как это физически невозможно
+            throw new InvalidArgumentException("Высота должна быть неотрицательной.");
+        }
+        $this->height = $height;
+    }
+}
+
+// Класс Circle представляет круг и реализует интерфейс Drawable
+class Circle extends Shape implements Drawable
+{
+    protected float $radius; // Радиус круга
+
+    // Конструктор для создания круга с заданным цветом и радиусом
+    public function __construct(string $color, float $radius)
+    {
+        if ($radius < 0) {
+            throw new InvalidArgumentException("Радиус должен быть неотрицательным.");
+        }
+        // Инициализация родительского класса для установки цвета фигуры
+        parent::__construct($color);
+        $this->radius = $radius;
+    }
+
+    // Реализация абстрактного метода для вычисления площади круга
+    public function calculateArea(): float
+    {
+        // Рассчитываем площадь круга
+        return pi() * pow($this->radius,2);
+    }
+
+    // Реализация метода рисования круга
+    public function draw(): void
+    {
+        // Выводим строковое представление объекта (информацию о фигуре)
+        echo "Рисуем: " . get_class($this) . ", цвет: " . $this->color . ", площадь: " . $this->formatArea() . "<br>";
+    }
+
+    // Метод для установки радиуса с проверкой на отрицательное значение
+    public function setRadius(float $radius): void
+    {
+        if ($radius < 0) {
+            // Выбрасываем исключение, если радиус отрицательный, так как это физически невозможно
+            throw new InvalidArgumentException("Радиус должен быть неотрицательным.");
+        }
+        $this->radius = $radius;
+    }
+
+}
+
+// Создание объекта прямоугольника с цветом "красный", шириной 10 и высотой 2
+$rectangle = new Rectangle("красный", 10, 2);
+// Создание объекта круга с цветом "синий" и радиусом 3
+$circle = new Circle("синий",3);
+
+// Вывод информации о фигурах
+$rectangle->draw();
+$circle->draw();
 

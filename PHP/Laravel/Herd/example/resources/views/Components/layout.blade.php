d231ac1be09d7ebdb23227210ef12cc05e5636e2
Ilnur Sirazhev <code_gex@mail.ru>
2025-01-23 16:39:05 +0500

    Laravel - шаблоны Blade, добавление компонента панели навигации

--- PHP/Laravel/Herd/example/resources/views/Components/layout.blade.php
+++ PHP/Laravel/Herd/example/resources/views/Components/layout.blade.php
@@ -9,6 +9,12 @@
 </head>
 <body>
 
+<nav>
+    <x-nav-link href="/">Home</x-nav-link>
+    <x-nav-link href="/about">About</x-nav-link>
+    <x-nav-link href="/contact">Contact</x-nav-link>
+</nav>
+
 {{ $slot }} {{--альтернатива команде <?php echo $slot ?>--}}
 
 </body>

a2730407e15a00f4b7a408283558b00d4575d152
Ilnur Sirazhev <code_gex@mail.ru>
2025-01-24 16:46:05 +0500

    Laravel - Autoloading, Namespaces, and Models

--- PHP/Laravel/Herd/example/routes/web.php
+++ PHP/Laravel/Herd/example/routes/web.php
@@ -1,62 +1,24 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
 
 Route::get('/', function () {
     return view('home');
 });
 
-//// Можно вернуть массив (полезно для использования API, данные передаются в виде массива
-//Route::get('/about', function () {
-//    return ['foo' => 'bar'];
-//});
 
 // Создание маршрута /jobs, который возвращает представление jobs
 Route::get('/jobs', function () {
     return view('jobs',[
-//        'greeting' => 'Hello', // эквивалент создания переменной $greeting = 'Hello'
-//        'name' => 'Yuriy Borisov'
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '40,000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '40,000'
-        ]
-    ];
+Route::get('/jobs/{id}', function ($id){
 
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 

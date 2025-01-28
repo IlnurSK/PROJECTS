a0a6e6ff72620d0a2f6621734032a6f37924fef4
Ilnur Sirazhev <code_gex@mail.ru>
2025-01-28 14:53:04 +0500

    Laravel - Знакомство с Factory, генерация данных в БД

--- PHP/Laravel/Herd/example/app/Models/Job.php
+++ PHP/Laravel/Herd/example/app/Models/Job.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job extends Model { // Eloquent автоматически будет искать таблицу JOBS в БД (которое занята по дефолту), в нашем случае она называется jobs_listings, поэтому класс можно переименовать например в JobListing (в ед. числе), ну или альтернативный вариант
+class Job extends Model {
+    use HasFactory; // Подключение библиотеки Фабрики (для генерации данных)
+
+    // Eloquent автоматически будет искать таблицу JOBS в БД (которое занята по дефолту), в нашем случае она называется jobs_listings, поэтому класс можно переименовать например в JobListing (в ед. числе), ну или альтернативный вариант
     protected $table = 'job_listings'; // алть. вариант - указание кокретной таблицы через $table = 'table_name
 
     // задание атрибутов которые можно заполнять массово

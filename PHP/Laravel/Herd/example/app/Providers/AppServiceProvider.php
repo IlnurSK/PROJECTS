a7539ca61e7f1560114784c91856540be6d2257f
Ilnur Sirazhev <code_gex@mail.ru>
2025-01-30 14:21:54 +0500

    Добавлен AppServiceProvider.php в текущую ветку

---
+++ PHP/Laravel/Herd/example/app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // Выключение использование метода отложенной загрузки
+        Model::preventLazyLoading();
+
+        // Настройка представления для PAGINATION
+//        Paginator::useBootstrapFive();
+
+    }
+}

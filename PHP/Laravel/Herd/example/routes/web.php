a2f84fbf1d0bceebea2d74fef18f98b143a9a8de
Ilnur Sirazhev <code_gex@mail.ru>
2025-02-04 14:53:51 +0500

    Laravel - Создание системы регистрации и авторизации

--- PHP/Laravel/Herd/example/routes/web.php
+++ PHP/Laravel/Herd/example/routes/web.php
@@ -1,37 +1,22 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('home');
-//});
 // Обновление метода отображения home страницы через view
 Route::view('/', 'home');
-
-// Группировка всех маршрутов ресурса Job
-//Route::controller(JobController::class)->group(function () {
-//    // Index.
-//    Route::get('/jobs', 'index');
-//    // Create.
-//    Route::get('/jobs/create', 'create');
-//    // Show.
-//    Route::get('/jobs/{job}', 'show');
-//    // Store.
-//    Route::post('/jobs', 'store');
-//    // Edit.
-//    Route::get('/jobs/{job}/edit', 'edit');
-//    // Update.
-//    Route::patch('/jobs/{job}', 'update');
-//    // Destroy.
-//    Route::delete('/jobs/{job}', 'destroy');
-//});
+// Практика: создание маршрута страницы контактов
+Route::view('/contact', 'contact');
 
 // Группировка всех маршрутов с помощью метода resource, по стандарту RESTful
 Route::resource('jobs', JobController::class);
 
-// Практика: создание маршрута страницы контактов
-//Route::get('/contact', function () {
-//    return view('contact');
-//});
-Route::view('/contact', 'contact');
+// Auth. Аутентификация
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+
